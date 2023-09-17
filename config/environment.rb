c0347198b150b1540a11cc0c6bbc941978099b3e
Jenovi Wayela <wjenovi@gmail.com>
2023-09-17 04:33:16 +0200

    watch-list2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+CLOUDINARY_URL = 'cloudinary://564186369325882:MnrXc6Ph-CXTJbvBZfsR5vTWUxQ@dvcgrzuzq'
--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,4 @@ Rails.application.routes.draw do
 
   resources :bookmarks, only: :destroy
 end
+
